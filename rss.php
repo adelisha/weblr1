25b6d558342e8c6ad5041954d52adb2723d181a5
Adelina Khayrullina <65246538+adelisha@users.noreply.github.com>
2021-04-29 00:23:24 +0500

    ЛР3 задание 1

---
+++ rss.php
@@ -0,0 +1,15 @@
+﻿<?php
+   $tek=$_GET["tek"];
+   $rss = simplexml_load_file("https://www.9tv.co.il/rss/mainfeed");      
+   $s=0;
+   foreach ($rss->channel->item as $item) {
+       $s++;
+       echo "<div style='width: 500px; border: 3px solid white; background-color: white'>";
+       echo '<h3 style="font-weight: bold">'.$item->title.'</h3>';
+       echo '<p>'.$item->description.'</p>'; 
+       echo "<img src=\"". (string)$item->enclosure['url'][0]. "\" style='width: 400px'></div><br>";
+       if($s==$tek) {
+          break;
+         }
+   }
+?>
