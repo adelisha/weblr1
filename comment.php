bd855dc7262ca13eaf94f253d3011d2bb868ca99
Adelina Khayrullina <65246538+adelisha@users.noreply.github.com>
2021-04-29 00:24:08 +0500

    ЛР3 задание2
    ЛР3 задание2

---
+++ comment.php
@@ -0,0 +1,30 @@
+﻿<?php
+
+
+$name=$_GET["name"];
+$email=$_GET["email"];
+$comment=$_GET["comment"];
+echo($email);
+$xml = simplexml_load_file("comment.xml"); 
+
+$cs = $xml->addChild('comments','');
+$cs->addChild("name", $name);
+$cs->addChild("email", $email);
+$cs->addChild("comment", $comment);
+$done = $xml->asXML("comment.xml");
+$schiotchik=0;
+$cn = count($xml->comments);
+$xml = simplexml_load_file("comment.xml");
+        for($i = 0; $i <= $cn; $i++){
+          $schiotchik++;
+          echo("<br><div style='border:  3px solid white; background-color: white'>");
+       echo "Имя: ".$xml->comments[$i]->name . "<br>";
+       echo "Почта: ".$xml->comments[$i]->email . "<br>";
+       echo "Отзыв: ".$xml->comments[$i]->comment . "<br>";
+       echo("</div>");
+       if($schiotchik==$cn)break;
+        }
+
+
+
+?>
---
+++ get.php
@@ -0,0 +1,14 @@
+﻿<?php
+$xml = simplexml_load_file("comment.xml");
+$tek = count($xml->comments);
+$s=0;
+for($i = 0; $i <= $tek; $i++){
+$s++;
+echo("<br><div style='border:  3px solid white; background-color: white'>");
+       echo "Имя: ".$xml->comments[$i]->name . "<br>";
+       echo "Почта: ".$xml->comments[$i]->email . "<br>";
+       echo "Отзыв: ".$xml->comments[$i]->comment . "<br></div>";
+       if($s==$tek){
+               break;}
+        }
+?>
